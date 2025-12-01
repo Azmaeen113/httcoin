91abcc226288296651e1f46ce3d8a1607f1fc9b5
Azmaeen113 <your-email@example.com>
2025-12-02 03:13:22 +0600

    aasd

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import MainLayout from "@/components/layout/MainLayout";
@@ -27,7 +27,7 @@ const App = () => (
         <BuyModalProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <HashRouter>
           <Routes>
             <Route element={<MainLayout />}>
               <Route path="/" element={<Index />} />
@@ -43,7 +43,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </BuyModalProvider>
       </WhitepaperModalProvider>
     </TooltipProvider>
